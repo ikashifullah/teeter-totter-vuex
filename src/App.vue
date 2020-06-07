4ae06ab1b98686d826202faa05cb624413b4012d
Kashif Ullah <ikashifullah@gmail.com>
2020-06-07 12:18:41 +0500

    Add initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Teeter Totter Vuex</h1>
+    <TeeterTotter msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import TeeterTotter from './components/TeeterTotter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    TeeterTotter,
   },
 };
 </script>
---
+++ src/components/TeeterTotter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <div class="game-height game-width d-inline-block">
+      <div class="space-above-scale"></div>
+      <div v-if="gameOver" style="color: orange; margin-top: 80px;">
+        <h3>Game Over</h3>
+        <div>Refresh page to Play again</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TeeterTotter',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.space-above-scale {
+  height: 300px;
+}
+.game-height {
+  height: 500px;
+  background-color: lightgrey;
+}
+.game-width {
+  width: 500px;
+}
+.d-inline-block {
+  display: inline-block;
+}
+</style>
